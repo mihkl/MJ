3ef86f20c0c1e461d929f2e92130be7153c39590
mihkl <mihkelj18@gmail.com>
2024-02-17 13:10:09 +0200

    html mallid

--- Pages/Controls/HtmlShowItem.cs
+++ Pages/Controls/HtmlShowItem.cs
@@ -2,6 +2,7 @@
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace MJ.Pages.Controls;
 
@@ -21,9 +22,37 @@ public static class HtmlShowItem {
         tagDd.WriteTo(writer, HtmlEncoder.Default);
 
         return new HtmlString(writer.ToString());
-
-
     }
+    public static IHtmlContent EditItem<TModel, TValue>(this IHtmlHelper<TModel> h, Expression<Func<TModel, TValue>> e) {
+
+        var lab = h.LabelFor(e, new { @class = "control-label" });
+        var ed = h.EditorFor(e, new { htmlAttributes = new { @class = "form-control" } });
+        var val = h.ValidationMessageFor(e, string.Empty, new { @class = "text-danger" });
+
+        var div = new TagBuilder("div");
+        div.AddCssClass("form-group");
+        div.InnerHtml.AppendHtml(lab);
+        div.InnerHtml.AppendHtml(ed);
+        div.InnerHtml.AppendHtml(val);
+
+        var writer = new StringWriter();
+        div.WriteTo(writer, HtmlEncoder.Default);
+
+        return new HtmlString(writer.ToString());
+    }
+    //public static IHtmlContent Index<TModel>(
+    //    this IHtmlHelper<TModel> htmlHelper,
+    //    params Expression<Func<TModel, object>>[] expressions) {
+    //    var tr = new TagBuilder("tr");
+
+    //    foreach (var expression in expressions) {
+    //        var th = new TagBuilder("th");
+    //        th.InnerHtml.AppendHtml(htmlHelper.DisplayNameFor(expression));
+    //        tr.InnerHtml.AppendHtml(th);
+    //    }
+
+    //    return tr;
+    //}
 
 }
 
