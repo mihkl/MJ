6f6ebf28e9db8d51ff0b760f37e952803403a4c1
mihkl <mihkelj18@gmail.com>
2024-03-02 03:55:10 +0200

    Parilikkuse algus

--- Soft/Models/Movie.cs
+++ Soft/Models/Movie.cs
@@ -10,12 +10,3 @@ public class Movie
     public string? Genre { get; set; }
     public decimal Price { get; set; }
 }
-
-public class Person {
-    public int Id { get; set; }
-    public string? FirstName { get; set; }
-    public string? FamilyName { get; set; }
-    [DataType(DataType.Date)] public DateTime DoB { get; set; }
-    public bool Gender { get; set; }
-    
-}
---
+++ Soft/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MJ.Soft.Models;
+
+public class Person {
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? FamilyName { get; set; }
+    [DataType(DataType.Date)] public DateTime DoB { get; set; }
+    public bool Gender { get; set; }
+    
+}
